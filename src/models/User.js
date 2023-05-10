896f7d20dbd4dd5768faef78c918453150dd2a2e
Marlon Couto <marloncouto199@gmail.com>
2023-05-10 16:56:08 -0300

    feat: adiciona model User

---
+++ src/models/User.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false,
+            type: DataTypes.INTEGER,
+        },
+        displayName: {
+            allowNull: false,
+            type: DataTypes.STRING,
+        },
+        email: {
+            allowNull: false,
+            unique: true,
+            type: DataTypes.STRING,
+        },
+        password: {
+            allowNull: false,
+            type: DataTypes.STRING,
+        },
+        image: {
+            allowNull: true,
+            type: DataTypes.STRING,
+        },
+    }, {
+        tableName: 'users',
+        timestamps: false,
+        underscored: true,
+    });
+
+    return User;
+};
