89ac6a44f7611e057c8ecfb4dfb43dc844677b78
Marlon Couto <marloncouto199@gmail.com>
2023-05-12 13:13:57 -0300

    style: atualiza formatação de app.js

--- src/app.js
+++ src/app.js
@@ -7,16 +7,7 @@ const {
     blogPostRoutes,
 } = require('./routes');
 
-// ...
-
 const app = express();
-
-app.use(express.json());
-
-app.use('/login', loginRoutes);
-app.use('/user', userRoutes);
-app.use('/categories', categoryRoutes);
-app.use('/post', blogPostRoutes);
 
 // não remova ou mova esse endpoint
 app.get('/', (_request, response) => {
@@ -25,7 +16,13 @@ app.get('/', (_request, response) => {
 
 app.use(express.json());
 
-// ...
+app.use('/login', loginRoutes);
+
+app.use('/user', userRoutes);
+
+app.use('/categories', categoryRoutes);
+
+app.use('/post', blogPostRoutes);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
